1d2c28a0fb6c0bc247c8f07d9b2c4a5d04cd77a7
glbitm-25113CN339 <guptaaryan956973@gmail.com>
2026-06-11 22:55:37 +0530

    day11

---
+++ day11/q4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+long factorial(int n)
+{
+    long fact = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d = %ld", num, factorial(num));
+
+    return 0;
+}
