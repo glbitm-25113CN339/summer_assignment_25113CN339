a0983fbbb20b682fdf33729ca0b6d86b68692811
glbitm-25113CN339 <guptaaryan956973@gmail.com>
2026-06-13 22:43:13 +0530

    day13

---
+++ day13/q4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, even = 0, odd = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even elements = %d\n", even);
+    printf("Odd elements = %d\n", odd);
+
+    return 0;
+}
